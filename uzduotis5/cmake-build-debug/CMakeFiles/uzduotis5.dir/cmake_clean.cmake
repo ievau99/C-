728b52b42256e8fc891234f31eca0f311d52242e
Ieva Blinstrubienė <ievau99@gmail.com>
2021-03-18 16:40:17 +0200

    Suvesti keturis Naturaliuosius skaicius

---
+++ uzduotis5/cmake-build-debug/CMakeFiles/uzduotis5.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/uzduotis5.dir/main.cpp.obj"
+  "uzduotis5.exe"
+  "uzduotis5.exe.manifest"
+  "uzduotis5.lib"
+  "uzduotis5.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/uzduotis5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
